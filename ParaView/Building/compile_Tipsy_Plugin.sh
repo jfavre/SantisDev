b4433069a1ec997b91cdef71c132e9221de7f5e2
jfavre <jfavre@users.noreply.github.com>
2024-05-14 13:49:28 +0200

    Update compile_Tipsy_Plugin.sh

--- ParaView/Building/compile_Tipsy_Plugin.sh
+++ ParaView/Building/compile_Tipsy_Plugin.sh
@@ -4,6 +4,10 @@ cdi_install_dir=$SCRATCH/GH/ParaView-CDI/cdi-v${cdi_version}Install
 catalyst_version=2.0.0
 catalyst_install_dir=$SCRATCH/GH/ParaView-CDI/catalyst-v${catalyst_version}Install
 export TBB_ROOT=`spack location -i intel-tbb/z5`
+
+export CC=`which gcc`
+export CXX=`which g++`
+
 ccmake -S .. \
   -BUILD_TESTING:BOOL=ON \
   -DParaView_DIR=/bret/scratch/cscs/jfavre/GH/ParaView/5.12-CDI/lib64/cmake/paraview-5.12 \
