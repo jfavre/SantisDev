d8ab5d606802687bbc71dd48fcc03c9789a8ee91
jfavre <jfavre@users.noreply.github.com>
2024-06-30 21:23:33 +0200

    Create build_paraview_todi_with_CDI.sh

---
+++ ParaView/Building/build_paraview_todi_with_CDI.sh
@@ -0,0 +1,5 @@
+cd $SCRATCH/GH/ParaView-CDI
+paraview_version=5.13.0-RC1
+paraview_install_dir=$SCRATCH/GH/ParaView/Todi-5.13
+wget https://www.paraview.org/files/v5.13/ParaView-v${paraview_version}.tar.xz
+tar xz ParaView-v${paraview_version}.tar.xz
