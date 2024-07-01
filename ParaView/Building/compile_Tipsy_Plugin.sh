8fe7d1736ba0b7e61f2130ac308d87ce28dccf79
jfavre <jfavre@users.noreply.github.com>
2024-07-01 09:40:43 +0200

    Update for Todi v5.13-RC1

--- ParaView/Building/compile_Tipsy_Plugin.sh
+++ ParaView/Building/compile_Tipsy_Plugin.sh
@@ -1,9 +1,9 @@
-cd /users/jfavre/Projects/Tipsy/ParaViewTipsyPlugin/build512
+cd /users/jfavre/Projects/Tipsy/ParaViewTipsyPlugin/build513
 cdi_version=2.2.4
 cdi_install_dir=$SCRATCH/GH/ParaView-CDI/cdi-v${cdi_version}Install
 catalyst_version=2.0.0
 catalyst_install_dir=$SCRATCH/GH/ParaView-CDI/catalyst-v${catalyst_version}Install
-export TBB_ROOT=`spack location -i intel-tbb/z5`
+export TBB_ROOT=`spack location -i intel-tbb`
 
 export CC=`which gcc`
 export CXX=`which g++`
@@ -11,8 +11,8 @@ export CXX=`which g++`
 cmake -S .. \
   -DBUILD_TESTING:BOOL=ON \
   -DCMAKE_BUILD_TYPE=Release \
-  -DParaView_DIR=/bret/scratch/cscs/jfavre/GH/ParaView/5.12-CDI/lib64/cmake/paraview-5.12 \
-  -Dcatalyst_DIR=${catalyst_install_dir}/lib64/cmake/catalyst-2.0 \
-  -DCDI_DIR=${cdi_install_dir}/lib/cmake/cdi
+  -DParaView_DIR=$SCRATCH/GH/ParaView/Todi-5.13/lib64/cmake/paraview-5.13 \
+  -DCDI_DIR=${cdi_install_dir}/lib/cmake/cdi \
+  -Dcatalyst_DIR=${catalyst_install_dir}/lib64/cmake/catalyst-2.0 
 
 make 
