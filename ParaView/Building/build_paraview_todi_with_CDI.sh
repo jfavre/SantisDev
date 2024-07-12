b1e7dcb4c9683ae5be42838718d17e6e4354834a
jfavre <jfavre@users.noreply.github.com>
2024-07-12 14:04:14 +0200

    adding same HDF5 used by netcdf build

--- ParaView/Building/build_paraview_todi_with_CDI.sh
+++ ParaView/Building/build_paraview_todi_with_CDI.sh
@@ -24,7 +24,8 @@ cmake -S ../ParaView-v${paraview_version} \
   -DMPI_C_COMPILER_INCLUDE_DIRS=/user-environment/env/default/include \
   -DCMAKE_BUILD_TYPE=Release \
   -DPARAVIEW_USE_FORTRAN:BOOL=ON \
-  -DVTK_MODULE_USE_EXTERNAL_VTK_hdf5:BOOL=OFF \
+  -DVTK_MODULE_USE_EXTERNAL_VTK_hdf5:BOOL=ON \
+  -DHDF5_DIR:PATH=`spack location -i hdf5/nj`/cmake \
   -DPARAVIEW_USE_MPI:BOOL=ON \
   -DPARAVIEW_BUILD_TESTING:BOOL=OFF \
   -DPARAVIEW_BUILD_EDITION=CANONICAL \
