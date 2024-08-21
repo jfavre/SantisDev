2ffb221221ade80ca9b477f84800da3ac2ad70ce
jfavre <jfavre@users.noreply.github.com>
2024-08-21 13:00:58 +0200

    fix hdf5 location

--- ParaView/Building/build_paraview_todi_with_CDI.sh
+++ ParaView/Building/build_paraview_todi_with_CDI.sh
@@ -41,7 +41,7 @@ cmake -S ../ParaView-v${paraview_version} \
   -DCMAKE_BUILD_TYPE=Release \
   -DPARAVIEW_USE_FORTRAN:BOOL=ON \
   -DVTK_MODULE_USE_EXTERNAL_VTK_hdf5:BOOL=ON \
-  -DHDF5_DIR:PATH=`spack location -i hdf5`/cmake \
+  -DHDF5_DIR:PATH=`spack location -i hdf5/aiimm22`/cmake \
   -DPARAVIEW_USE_MPI:BOOL=ON \
   -DPARAVIEW_BUILD_TESTING:BOOL=OFF \
   -DPARAVIEW_BUILD_EDITION=CANONICAL \
@@ -88,7 +88,7 @@ cmake -S ../ParaView-v${paraview_version} \
   -DCMAKE_BUILD_TYPE=Release \
   -DPARAVIEW_USE_FORTRAN:BOOL=ON \
   -DVTK_MODULE_USE_EXTERNAL_VTK_hdf5:BOOL=ON \
-  -DHDF5_DIR:PATH=`spack location -i hdf5`/cmake \
+  -DHDF5_DIR:PATH=`spack location -i hdf5/aiimm22`/cmake \
   -DPARAVIEW_USE_MPI:BOOL=ON \
   -DPARAVIEW_BUILD_TESTING:BOOL=OFF \
   -DPARAVIEW_BUILD_EDITION=CANONICAL \
