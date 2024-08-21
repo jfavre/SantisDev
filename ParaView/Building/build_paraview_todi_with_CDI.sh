f14106e03407b6dc8588ebc14e42ba114c8b669f
jfavre <jfavre@users.noreply.github.com>
2024-08-21 12:50:47 +0200

    adding a recipe to build with ospray 3.1

--- ParaView/Building/build_paraview_todi_with_CDI.sh
+++ ParaView/Building/build_paraview_todi_with_CDI.sh
@@ -73,3 +73,42 @@ cmake -S ../ParaView-v${paraview_version} \
   -Drkcommon_DIR=`spack location -i rkcommon/gxwzatp`/lib64/cmake/rkcommon-1.11.0 \
   -Dopenvkl_DIR=`spack location -i openvkl/tgvxiwb`/lib64/cmake/openvkl-1.3.2 \
   -Dospray_DIR=`spack location -i ospray/jhobziu`/lib64/cmake/ospray-2.12.0
+
+# Adding a new build for ospray v3.1
+
+paraview_version=5.13.0-RC2
+paraview_install_dir=$SCRATCH/GH/ParaView/Todi-5.13-Ospray31
+mkdir ParaView-v${paraview_version}Build-EGL-Ospray31
+cd    ParaView-v${paraview_version}Build-EGL-Ospray31
+
+cmake -S ../ParaView-v${paraview_version} \
+  -DCMAKE_INSTALL_PREFIX=${paraview_install_dir} \
+  -DMPI_C_COMPILER=/user-environment/env/default/bin/mpicc \
+  -DMPI_C_COMPILER_INCLUDE_DIRS=/user-environment/env/default/include \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPARAVIEW_USE_FORTRAN:BOOL=ON \
+  -DVTK_MODULE_USE_EXTERNAL_VTK_hdf5:BOOL=ON \
+  -DHDF5_DIR:PATH=`spack location -i hdf5`/cmake \
+  -DPARAVIEW_USE_MPI:BOOL=ON \
+  -DPARAVIEW_BUILD_TESTING:BOOL=OFF \
+  -DPARAVIEW_BUILD_EDITION=CANONICAL \
+  -DPARAVIEW_USE_PYTHON:BOOL=ON \
+  -DVTK_SMP_IMPLEMENTATION_TYPE:STRING=TBB \
+  -DTBB_DIR=`spack location -i intel-tbb`/lib64/cmake/TBB \
+  -DPARAVIEW_ENABLE_EXAMPLES:BOOL=OFF \
+  -DPARAVIEW_USE_QT:BOOL=OFF \
+  -DPARAVIEW_ENABLE_WEB:BOOL=OFF \
+  -DVTK_OPENGL_HAS_EGL:BOOL=ON -DVTK_USE_X:BOOL=OFF \
+  \
+  -DPARAVIEW_PLUGIN_ENABLE_pvNVIDIAIndeX:BOOL=ON \
+  -DPARAVIEW_PLUGIN_AUTOLOAD_pvNVIDIAIndeX:BOOL=ON \
+  \
+  -DPARAVIEW_USE_VTKM=ON -DPARAVIEW_USE_CUDA=ON -DCMAKE_CUDA_ARCHITECTURES=90 \
+  \
+  -DPARAVIEW_ENABLE_RAYTRACING:BOOL=ON \
+  -DVTKOSPRAY_ENABLE_DENOISER:BOOL=ON \
+  -DOpenImageDenoise_DIR=`spack location -i openimagedenoise`/lib64/cmake/OpenImageDenoise-2.2.2 \
+  -Dembree_DIR=`spack location -i embree`/lib64/cmake/embree-4.3.1 \
+  -Drkcommon_DIR=`spack location -i rkcommon/lnwwsxc`/lib64/cmake/rkcommon-1.11.0 \
+  -Dopenvkl_DIR=`spack location -i openvkl/qjcbccz`/lib64/cmake/openvkl-1.3.2 \
+  -Dospray_DIR=`spack location -i ospray/ck2oipm`/lib64/cmake/ospray-2.12.0
