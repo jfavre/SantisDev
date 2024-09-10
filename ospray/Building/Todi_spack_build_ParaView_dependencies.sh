2162c6c4bb9847486694d0fa6f7fb481583ad2fb
jfavre <jfavre@users.noreply.github.com>
2024-09-10 16:12:53 +0200

    adding new version of CDI

--- ospray/Building/Todi_spack_build_ParaView_dependencies.sh
+++ ospray/Building/Todi_spack_build_ParaView_dependencies.sh
@@ -16,13 +16,18 @@ export CXX=`which g++`
 cdi_version=2.2.4
 cdi_install_dir=$SCRATCH/GH/ParaView-CDI/cdi-v${cdi_version}Install
 wget https://code.mpimet.mpg.de/attachments/download/28877/cdi-2.2.4.tar.gz
-gunzip cdi-2.2.4.tar.gz
-tar xf cdi-2.2.4.tar
-cd cdi-2.2.4
+gunzip cdi-${cdi_version}.tar.gz
+tar xf cdi-${cdi_version}.tar
+cd cdi-${cdi_version}
 pushd `spack location -i eccodes`
 ln -s lib64 lib
 popd
 ./configure --enable-iso-c-interface --with-netcdf=`spack location -i netcdf-c` --with-eccodes=`spack location -i eccodes` --prefix=${cdi_install_dir} --enable-grib=no
 make && make install
+
+# adding a new version to test
+cdi_version=2.4.0
+cdi_install_dir=$SCRATCH/GH/ParaView-CDI/cdi-v${cdi_version}Install
+wget https://code.mpimet.mpg.de/attachments/download/29309/cdi-${cdi_version}.tar.gz
 
 
