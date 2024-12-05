43004ff8032cbdf06abef8b1bbec7fa3777bc582
jfavre <jfavre@users.noreply.github.com>
2024-12-05 08:14:21 +0100

    using prgenv-gnu/24.11:v1

--- VMD/Testing/vmd194_egl.sh
+++ VMD/Testing/vmd194_egl.sh
@@ -12,7 +12,7 @@ export VMDDIR=$HOME/Projects/VMD/lib
 #srun -n 1 -N 1 vmd -size 1920 1080 -dispdev openglpbuffer -eofexit < rendering_OGL.tcl
 export VMDSCRSIZE=1920
 
-srun --uenv=/bret/scratch/cscs/bcumming/images/prgenv-gnu-24.2-v2.squashfs:/user-environment -u -N 1 -n 1 vmd -size 1920 1080 -dispdev openglpbuffer -eofexit < rendering_OGL.tcl
+srun --uenv=prgenv-gnu/24.11:v1:/user-environment -u -N 1 -n 1 vmd -size 1920 1080 -dispdev openglpbuffer -eofexit < rendering_OGL.tcl
 #
 #srun -n 1 -N 1 vmd -size 1920 1080 -dispdev none -eofexit < renderings_Tachyon1.tcl
 
